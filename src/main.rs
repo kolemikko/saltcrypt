02025a63f13e38ad2867f983987b36f576170ca4
kolemikko <mikko.kolehmainen@tuta.io>
2021-12-22 20:04:12 +0200

    Changed argument ordering and prepared main for gui mode

--- src/main.rs
+++ src/main.rs
@@ -4,64 +4,65 @@ mod core;
 fn main() {
     let args = App::new("Saltcrypt")
         .version(crate_version!())
-        .about("In-place file encryption/decryption tool")
+        .about("In-place file encryption tool")
         .author("Mikko Kolehmainen")
         .arg(
-            Arg::with_name("filepath")
+            Arg::with_name("mode")
                 .index(1)
+                .possible_values(&["e", "d"])
+                .requires("filepath")
+                .help("Choose encryption or decryption mode"),
+        )
+        .arg(
+            Arg::with_name("filepath")
+                .index(2)
                 .short("f")
                 .long("filepath")
                 .takes_value(true)
-                .required(true)
                 .requires("password")
-                .help("Path to a file to be encrypted/decrypted"),
+                .help("Path to a file"),
         )
         .arg(
             Arg::with_name("password")
-                .index(2)
+                .index(3)
                 .short("p")
                 .long("password")
                 .takes_value(true)
-                .required(true)
                 .requires("salt")
                 .help("Password to be used"),
         )
         .arg(
             Arg::with_name("salt")
-                .index(3)
+                .index(4)
                 .short("s")
                 .long("salt")
                 .takes_value(true)
-                .required(true)
                 .requires("mode")
-                .help("Salt to be used for extra security"),
-        )
-        .arg(
-            Arg::with_name("mode")
-                .index(4)
-                .possible_values(&["e", "d"])
-                .required(true)
-                .help("Choose encryption or decryption mode"),
+                .help("Salt to be used"),
         )
         .get_matches();
 
-    match args.value_of("mode").unwrap() {
-        "e" => {
-            core::encrypt_file(
-                args.value_of("filepath").unwrap(),
-                args.value_of("password").unwrap(),
-                args.value_of("salt").unwrap(),
-            )
-            .unwrap();
+    if args.is_present("mode") {
+        match args.value_of("mode").unwrap() {
+            "e" => {
+                core::encrypt_file(
+                    args.value_of("filepath").unwrap(),
+                    args.value_of("password").unwrap(),
+                    args.value_of("salt").unwrap(),
+                )
+                .unwrap();
+            }
+            "d" => {
+                core::decrypt_file(
+                    args.value_of("filepath").unwrap(),
+                    args.value_of("password").unwrap(),
+                    args.value_of("salt").unwrap(),
+                )
+                .unwrap();
+            }
+            _ => unreachable!(),
         }
-        "d" => {
-            core::decrypt_file(
-                args.value_of("filepath").unwrap(),
-                args.value_of("password").unwrap(),
-                args.value_of("salt").unwrap(),
-            )
-            .unwrap();
-        }
-        _ => unreachable!(),
+    } else {
+        println!("Should start gui here");
     }
 }
